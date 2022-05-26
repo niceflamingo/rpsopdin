cade175fe9d9f1c1e87a274ccf599818a24e858c
niceFlamingo <104111513+niceflamingo@users.noreply.github.com>
2022-05-26 14:52:10 -0700

    removed old commented out code & play functions

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let playerScore = 0
 function game() {
   for (let i = 0; i < 5; i++) {
     // look up how to not count a "round" if a value other than rock/paper/scissors has been added
-    let computerPlay = function () {
+    function computerPlay() {
       const choices = ["rock", "paper", "scissors"]
       return choices[Math.floor(Math.random() * choices.length)]
     }
@@ -18,9 +18,9 @@ function game() {
     } // look up how to add a loop that makes it so that if another word is entered it prompts to user to type rock/paper/scissors
     // while or for? maybe
 
-    playerSelection = playerInput()
+    let playerSelection = playerInput()
 
-    let playRound = function (playerSelection, computerSelection) {
+    function playRound(playerSelection, computerSelection) {
       if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore += 1
         console.log(
@@ -93,97 +93,3 @@ function endMessage() {
         `¯\_(ツ)_/¯, you lost to the computer. Your score: ${playerScore}. Computer's Score: ${computerScore}`
       )
 }
-
-// const computerSelection = computerPlay();
-// console.log(computerSelection);
-
-// const playerSelection = playerInput();
-// console.log(playerSelection);
-
-// const round = playRound();
-// console.log(round);
-
-// let computerScore = 0;
-// let playerScore = 0;
-
-// function computerPlay() {
-//   const choices = ["rock", "paper", "scissors"];
-//   return choices[Math.floor(Math.random() * choices.length)];
-// }
-
-// function playerInput() {
-//   let playerChoice = prompt("Choose Rock Paper or Scissors!");
-
-//   if (
-//     playerChoice.toLowerCase() === "rock" ||
-//     playerChoice.toLowerCase() === "paper" ||
-//     playerChoice.toLowerCase() === "scissors"
-//   ) {
-//     return playerChoice;
-//   } else if (
-//     playerChoice.toLowerCase() !== "rock" ||
-//     playerChoice.toLowerCase() !== "paper" ||
-//     playerChoice.toLowerCase() !== "scissors"
-//   ) {
-//     prompt("Please enter your choice again: Rock Paper or Scissors");
-//   } else {
-//     prompt("Try reloading the page to try again.");
-//   }
-//   return playerChoice;
-// // }
-// // add null statement that reloads or something when value comes back undefined
-
-// function playRound(playerSelection, computerSelection) {
-//   if (playerSelection === computerSelection) {
-//     return "It's a tie!";
-//   } else if (playerSelection === "rock" && computerSelection === "paper") {
-//     return "Sorry, you lose! Paper beats rock.";
-//   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-//     return "You win! Scissors beats rock.";
-//   } else if (playerSelection === "paper" && computerSelection === "rock") {
-//     return "You win! Paper beats rock.";
-//   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-//     return "Sorry, you lose! Scissors beats paper";
-//   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-//     return "You win! Scissors beats paper";
-//   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-//     return "Sorry, you lose! Rock beats scissors";
-//   }
-// }
-// console.log(playRound("rock", "paper"));
-
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     function playRound(playerSelection, computerSelection) {
-//       if (playerSelection === computerSelection) {
-//         return "It's a tie!";
-//       } else if (playerSelection === "rock" && computerSelection === "paper") {
-//         return "Sorry, you lose! Paper beats rock.";
-//       } else if (
-//         playerSelection === "rock" &&
-//         computerSelection === "scissors"
-//       ) {
-//         return "You win! Scissors beats rock.";
-//       } else if (playerSelection === "paper" && computerSelection === "rock") {
-//         return "You win! Paper beats rock.";
-//       } else if (
-//         playerSelection === "paper" &&
-//         computerSelection === "scissors"
-//       ) {
-//         return "Sorry, you lose! Scissors beats paper";
-//       } else if (
-//         playerSelection === "scissors" &&
-//         computerSelection === "paper"
-//       ) {
-//         return "You win! Scissors beats paper";
-//       } else if (
-//         playerSelection === "scissors" &&
-//         computerSelection === "rock"
-//       ) {
-//         return "Sorry, you lose! Rock beats scissors";
-//       }
-//     }
-//     playRound(playerSelection, computerSelection);
-//   }
-// }
-// game();
